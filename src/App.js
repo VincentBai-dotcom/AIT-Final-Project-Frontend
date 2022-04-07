9e42e867dce5bf3000c7761f183c0e7f220b2b1f
Vincent Bai <butcheryyy@gmail.com>
2022-04-06 20:51:08 -0400

    initial test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [correctNum, setCorrectNum] = useState(0);
+  const [num, setNum] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => {setNum(num + 1);setCorrectNum(correctNum+1)}}>Right answer</button>
+      <button onClick={() => {setNum(num + 1);}}>Wrong answer</button>
+      <p>Stats: {correctNum} / {num} </p>
     </div>
   );
 }
