7be730bc1f7f5ee0864487ad256dbf710ddfc8ec
Vincent Bai <butcheryyy@gmail.com>
2022-04-25 22:27:41 -0400

    login and signup message

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,9 @@ import { useNavigate } from 'react-router-dom';
 
 function Login(){
     const history = useNavigate();
-    function logIn(){
+
+    const [msg, setMsg] = useState("");
+    function HandleLogIn(){
         api.post('login/',{
             password: password,
             username: username
@@ -16,24 +18,28 @@ function Login(){
             history('/');
             window.location.reload(false);
         })
-        .catch((err)=>{console.log(err)});
+        .catch((err)=>{setMsg(err.response.data.msg)});
     };
+
+    function handleSignUP(){
+        api.post('signup/', {
+            username: username,
+            password: password
+        })
+        .then(res => {console.log(res); setMsg(res.data.msg)})
+        .catch(err => {console.log(err);})
+    }
     
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    function get(){
-        api.get('test/')
-        .then((res)=>{
-        console.log(res);
-        })
-    }
+    
     return(
         <div>
-        <input type="text" value={username} onChange={(e) => {setUsername(e.target.value)}} />
-        <input type="password" value={password} onChange={(e) => {setPassword(e.target.value)}} />
-        <button onClick= {logIn}>Login</button>
-    
-        <button onClick = {get}>get</button>
+        <input type="text" value={username} onChange={(e) => {setUsername(e.target.value)}} /> <br/>
+        <input type="password" value={password} onChange={(e) => {setPassword(e.target.value)}} /><br/>
+        <button onClick= {HandleLogIn}>Log in</button>
+        <button onClick= {handleSignUP}>Sign up</button>
+        <p>{msg}</p>
         </div>
         
     );
