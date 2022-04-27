2ed5151673a4c3b09001dd2a9054a2ba046cf8a6
Vincent Bai <butcheryyy@gmail.com>
2022-04-27 18:46:59 -0400

    using local api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://smart-ear-trainer-api.herokuapp.com/'
+    baseURL: 'http://localhost:5000/'
 });
 
 api.interceptors.request.use(
